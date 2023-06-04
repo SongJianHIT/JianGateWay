26f4064f4476b4e0e4ebafb73c3f2c3075e039cf
宋健 <2582231611@qq.com>
2023-06-04 11:18:32 +0800

    构建bootstrap网关启动类

---
+++ gateway-core/src/main/java/tech/songjian/core/Bootstrap.java
@@ -0,0 +1,31 @@
+/**
+ * @projectName JianGateWay
+ * @package tech.songjian.core
+ * @className tech.songjian.core.Bootstrap
+ */
+package tech.songjian.core;
+
+/**
+ * Bootstrap
+ * @description API网关启动类
+ * @author SongJian
+ * @date 2023/6/4 11:13
+ * @version
+ */
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        // 1、加载网关的核心静态配置
+
+        // 2、插件初始化
+
+        // 3、配置中心管理器初始化：连接配置中心，监听配置中心的新增、修改、删除
+
+        // 4、启动容器
+
+        // 5、连接注册中心，将注册中心的实例加载到本地
+
+        // 6、服务优雅关机
+    }
+}
+
